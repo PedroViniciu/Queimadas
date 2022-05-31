cbdfd8048c50406097b398268bfe92e28d5e8373
Pedro Vinicius <88795267+PedroViniciu@users.noreply.github.com>
2022-05-31 14:00:09 -0300

    Primeiros arquivos do pré-projeto

---
+++ mobile-navbar.js
@@ -0,0 +1,7 @@
+class MoblieNavbar{
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = documen.querySelectorAll(navLinks);
+    }
+}
