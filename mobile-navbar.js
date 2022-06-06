282e4b81086b9642a7ca1ba1e3f05cfc9aadcce3
Pedro Vinicius <88795267+PedroViniciu@users.noreply.github.com>
2022-06-06 14:01:20 -0300

    Arquivos mais avançados com CSS

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,7 +1,43 @@
-class MoblieNavbar{
+
+//Criação da responsividade do código
+class mobileNavbar{
     constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = documen.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
     }
-}
+
+    animateLinks(){
+        this.navLinks.forEach((link) =>{
+            link.style.animation
+                ? (link.style.animation = "" )
+                : (link.style.animation )
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+} 
+
+const mobileNavbar = new mobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
